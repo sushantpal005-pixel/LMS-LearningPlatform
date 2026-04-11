55128d895f2ce7a1986968093ce3aaa06c19af1d
sushantpal005-pixel <sushantpal005@gmail.com>
2026-04-12 02:16:26 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button>Let's Build LMS</Button>
+    </>
+  )
+}
+
+export default App
